57832d83389f68df1d168171ff1ed77376866344
proMahadi <professionalmahadi@gmail.com>
2025-01-15 12:09:29 +0600

    apollo client added

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "../App";
 import { HelmetProvider } from "react-helmet-async";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
 
 const RouterApp = () => {
   return (
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 };
 
