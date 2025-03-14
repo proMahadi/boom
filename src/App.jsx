c6c32d95b623624e4804aad49edd7f5922ed8463
proMahadi <professionalmahadi@gmail.com>
2025-03-14 10:52:47 +0600

    home page cleared to experiment more problems and new things

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
         <Routes>
-          <Route path="/" element={<Map/>} />
+          <Route path="/" element={<Home/>} />
           <Route path="about" element={<About />} />
         </Routes>
     </>
