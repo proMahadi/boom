bc9f926fe827c93e4b7af86b8147964c87e81ced
proMahadi <professionalmahadi@gmail.com>
2025-03-18 16:52:25 +0600

    area chart learned

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DoughnutChart from '../components/Doughnut'
+import AreaChart from '../components/Area'
 
 const Home = () => {
   return (
     <div>
-      <DoughnutChart/>
+      {/* <DoughnutChart/> */}
+      <AreaChart/>
     </div>
   )
 }
